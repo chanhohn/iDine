fc08203e5dea4af0abc243e6d26c50723c7c54a2
chanhohn <ciwei17@gmail.com>
2024-11-04 21:35:18 +0800

    add ObservableObject

---
+++ iDine/CheckOutView.swift
@@ -0,0 +1,32 @@
+//
+//  CheckOutView.swift
+//  iDine
+//
+//  Created by hohn on 2024/11/4.
+//
+
+import SwiftUI
+
+struct CheckOutView: View {
+    @EnvironmentObject var order: Order
+    let payTypes = ["Cash", "weChatPay", "AliPay", "iDentCoin"]
+    @State private var payType = "weChatPay"
+
+    var body: some View {
+        VStack {
+            Section {
+                Picker("How do you want to pay", selection: $payType) {
+                    ForEach(payTypes, id: \.self) {
+                        Text($0)
+                    }
+                }
+            }
+        }
+        .navigationTitle("payment")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    CheckOutView().environmentObject(Order())
+}
