21aa096e253c41f11580954c94950d2a0128bf32
chanhohn <ciwei17@gmail.com>
2024-11-06 20:17:35 +0800

    add ObservableObject

--- iDine/CheckOutView.swift
+++ iDine/CheckOutView.swift
@@ -9,8 +9,12 @@ import SwiftUI
 
 struct CheckOutView: View {
     @EnvironmentObject var order: Order
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
     let payTypes = ["Cash", "weChatPay", "AliPay", "iDentCoin"]
     @State private var payType = "weChatPay"
+    let tipAmounts = [10, 15, 20, 25, 0]
+    @State private var tipAmount = 15
 
     var body: some View {
         Form {
@@ -19,6 +23,27 @@ struct CheckOutView: View {
                     ForEach(payTypes, id: \.self) {
                         Text($0)
                     }
+                }
+            }
+            
+            Toggle("Add iDine loyalty card", isOn: $addLoyaltyDetails.animation())
+//           当用户打开开关的时候才显示填写会员卡的视图
+            if addLoyaltyDetails {
+                TextField("Enter your iDine ID", text: $loyaltyNumber)
+            }
+            
+            Section("Add a tip?") {
+                Picker("Percentage:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Total: $100") {
+                Button("Confirm order") {
+                    // place the order
                 }
             }
         }
